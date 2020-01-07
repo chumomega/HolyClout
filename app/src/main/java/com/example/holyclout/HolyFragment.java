d1b2f9956154fb14ee7f8c28989aed20c8e03f99
Chuma Okoro <cokoro@etsy.com>
2020-01-07 09:09:59 -0500

    wire up date button and title in fragment code

--- app/src/main/java/com/example/holyclout/HolyFragment.java
+++ app/src/main/java/com/example/holyclout/HolyFragment.java
@@ -1,4 +1,57 @@
 package com.example.holyclout;
 
-public class HolyFragment {
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HolyFragment extends Fragment {
+    private Holydeed holyDeed;
+    private EditText titleField;
+    private Button dateButton;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        holyDeed = new Holydeed();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_holy, container, false);
+        //the activity will add the view, it doesn't need to be done here
+        titleField = view.findViewById(R.id.holy_title);
+        titleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                holyDeed.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        dateButton = view.findViewById(R.id.holy_act_date);
+        dateButton.setText(holyDeed.getDate().toString());
+        dateButton.setEnabled(false);
+
+        return view;
+    }
 }
