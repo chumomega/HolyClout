e0a0c6403593733dda3caa996176df34b5bdcd5f
Chuma Okoro <cokoro@etsy.com>
2020-01-07 09:42:39 -0500

    display fragment on the main activity

--- app/src/main/java/com/example/holyclout/HolyActivity.java
+++ app/src/main/java/com/example/holyclout/HolyActivity.java
@@ -1,6 +1,8 @@
 package com.example.holyclout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,17 @@ public class HolyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_holy);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.frag_container);
+
+        if(fragment == null){
+            fragment = new HolyFragment();
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frag_container, fragment)
+                .commit();
+
+        }
     }
 }
